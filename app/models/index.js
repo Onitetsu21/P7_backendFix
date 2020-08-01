d6753df51c74b8720915b5d35263594da8c0f116
Oni <alix.boufflers@gmail.com>
2020-08-01 20:50:33 +0200

    last fix

--- app/models/index.js
+++ app/models/index.js
@@ -7,7 +7,6 @@ const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || "development";
 const config = require("../config/config.json")[env];
 const db = {};
-// Option 1: Passing parameters separately const sequelize = new Sequelize('database', 'username', 'password', { host: 'localhost', dialect: /* one of 'mysql' | 'mariadb' | 'postgres' | 'mssql' */ });
 
 const sequelize = new Sequelize(
   config.database,
